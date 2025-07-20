698eae4991fe2f40d9310e8a937f0e2a002ac286
mujahidraj <mujahidraj65@gmail.com>
2025-07-21 00:25:14 +0600

    practice task

---
+++ Module-17/practice/17.1.js
@@ -0,0 +1,17 @@
+var orange = 500;
+
+var apples = 300;
+
+var total = orange + apples;
+
+console.log("My money : " + myMoney);
+
+console.log("total cost : " + total);
+
+var myMoney = 1000;
+
+
+
+var returnMoney = myMoney-total;
+
+console.log("Return Money : "+ returnMoney)
