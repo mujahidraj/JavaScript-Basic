d55b3f28008ea0ff82bd7e6530d81f0dd6431781
mujahidraj <mujahidraj65@gmail.com>
2025-07-22 01:00:49 +0600

    18.3

--- Module-18/18.3.js
+++ Module-18/18.3.js
@@ -11,3 +11,21 @@ Create a simple JavaScript program that takes a student's score as input and ret
     F: 0-59
 
 ***/
+
+let studentGrade = 67;
+
+if(studentGrade >= 90 && studentGrade <= 100){
+    console.log("Congratulation you got A with " + studentGrade);
+}
+else if (studentGrade>=80 && studentGrade<=89){
+    console.log("Congratulation you got B with " + studentGrade);
+}
+else if (studentGrade>=70 && studentGrade<=79){
+    console.log("congratulation you got C with " + studentGrade);
+}
+else if (studentGrade>=60 && studentGrade<=69){
+    console.log("console to you. You got D with " + studentGrade);
+}
+else {
+    console.log("You are failed");
+}
