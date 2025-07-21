ed4cb1287b0f7eb5a1bf445ed18eda8777e17a24
mujahidraj <mujahidraj65@gmail.com>
2025-07-22 00:42:34 +0600

    18.1

--- Module-18/18.1.js
+++ Module-18/18.1.js
@@ -4,3 +4,15 @@ Free Drinks
     - Burger more than 500tk: free Coke
     - Else Coke: 30tk
 */
+
+
+let burger = 450;
+
+if(burger>=500){
+    // free coke
+    console.log("Congratulation you got a free coke of 30 Taka.")
+}
+else {
+    // no free coke
+    console.log("Pay 30Taka for the coke .")
+}
