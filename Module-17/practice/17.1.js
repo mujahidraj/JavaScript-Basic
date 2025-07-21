dc7873a7b96c6144e1f8a48bc1e6572ffae3421f
mujahidraj <mujahidraj65@gmail.com>
2025-07-22 01:28:10 +0600

    17.1

--- Module-17/practice/17.1.js
+++ Module-17/practice/17.1.js
@@ -4,11 +4,11 @@ var apples = 300;
 
 var total = orange + apples;
 
+var myMoney = 1000;
+
 console.log("My money : " + myMoney);
 
 console.log("total cost : " + total);
-
-var myMoney = 1000;
 
 
 
