0a019ec64190ff4cd8763eb3f9d31d27c6a55bd8
mujahidraj <mujahidraj65@gmail.com>
2025-07-22 18:44:26 +0600

    18.5.2

---
+++ Module-18.5/18.5.2.js
@@ -0,0 +1,19 @@
+// ### 2. Add or remove elements
+// 1. Declare an array of 3 tourist destinations
+// 2. Add a new tourist destination to your tourist array
+// 3. Add two more to your array
+// 4. Remove the last tourist destination you have added
+// 5. display the final array as output
+
+let destination = ["Sajek Valley" , "Sylhet" , "Sundarban"]
+
+destination.push("Saint martin");
+
+destination.push("Rangamati", "Bandarban");
+
+console.log(destination);
+
+destination.pop();
+
+console.log(destination)
+
