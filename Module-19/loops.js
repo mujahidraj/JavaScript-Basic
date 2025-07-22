8ba3ab34ff531d341c0ab93a11193967ad4e26ab
mujahidraj <mujahidraj65@gmail.com>
2025-07-23 01:01:34 +0600

    Fundamental concept of loops

---
+++ Module-19/loops.js
@@ -0,0 +1,42 @@
+let array = [1, 2, 3, 4, 5];
+
+// Using a for loop
+// for(const num of array){
+//     console.log(num);
+// }
+
+const fruits = ['apple', 'banana', 'cherry'];
+
+//Using a for loop
+// for(const fruit of fruits){
+//     console.log(fruit);
+// }
+
+
+//odd number printing
+
+// let numbers=1;
+
+// while(numbers<=100){
+//     console.log(numbers);
+//     numbers+=2;
+// }
+
+//even number printing
+
+// let numbers1=0;
+
+// while(numbers1<=100){
+//     console.log(numbers1)
+//     numbers1+=2;
+// }
+
+// sum of the numbers
+// let number=0;
+// let sum=0;
+
+// while (number<=10){
+//     sum=sum+number;
+//     number++;
+// }
+// console.log(sum)
