64fbba8167107c251d717c239a1bba63eb9371a5
mujahidraj <mujahidraj65@gmail.com>
2025-07-23 01:32:11 +0600

    19.4

---
+++ Module-19/19.4.js
@@ -0,0 +1,17 @@
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+
+let numbers = 0;
+
+let multiplication =0;
+
+while(numbers<=10){
+    multiplication = 5*numbers;
+    console.log("5 X", numbers ,"=" ,multiplication)
+    numbers++
+}
+
+
