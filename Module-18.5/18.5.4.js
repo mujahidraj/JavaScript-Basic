5d15b3e8e1ef637c5fc0b6b4158c2efbbc2f6364
mujahidraj <mujahidraj65@gmail.com>
2025-07-22 18:52:21 +0600

    18.5.4

---
+++ Module-18.5/18.5.4.js
@@ -0,0 +1,25 @@
+// ### 4. Checking if it's an Array
+
+// **Instructions:**
+
+// 1. Create different variables, each containing either an array or a non-array value.
+
+// 2. Now use isArray to check if each variable is an array.
+
+// 3. Print a message to the console indicating whether each variable is an array or not.
+
+let names="Raj";
+
+let age = 34;
+
+let items =[2 ,3 ,4 ,5 ,6];
+
+let characters = "[]";
+
+console.log(Array.isArray(age))
+
+console.log(Array.isArray(characters))
+
+console.log(Array.isArray(names))
+
+console.log(Array.isArray(items))
