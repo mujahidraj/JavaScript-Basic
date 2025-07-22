59c8c1b4aac087e930a4dcf7b8176b9cbcf2e3d1
mujahidraj <mujahidraj65@gmail.com>
2025-07-22 18:56:13 +0600

    18.5.5

---
+++ Module-18.5/18.5.5.js
@@ -0,0 +1,15 @@
+// ### 5. Combining Arrays
+
+// **Instructions:**
+
+// 1. Create two arrays of your choice.
+// 2. Use the concat method to combine the two arrays into a new array.
+// 3. Print both the original arrays and the combined array using console.log().
+
+let fruits = ["Mango" , "Apple" , "Banana" ,"Liche" ];
+
+let languages= ["Bangla","English" , "Urdu" , "French" , "Japanese"]
+
+let anything = fruits.concat(languages);
+
+console.log(anything);
