90c4cdb74ed92fdf7f276715e7fb4616eb43f995
mujahidraj <mujahidraj65@gmail.com>
2025-07-22 18:12:21 +0600

    Documentation

---
+++ Module-18.5/array.js
@@ -0,0 +1,49 @@
+const array = [12 ,23 ,34 ,45 ,56]
+
+// get total array
+// console.log(array)
+
+const names = ["Raj" , "Mayad" ,"Fidbi" , "Alin"]
+
+// console.log(names)
+
+// get specific array
+// console.log(names[1])
+
+// get array length
+// console.log(array.length)
+
+// set index value
+
+let name5= "Nadiya";
+
+names[3]="Raina";
+names[4]=name5;
+
+// console.log(names[3])
+
+console.log(names)
+
+//injecting element in a array
+names.push("Auntu");
+names.push("Jane", "Jahangir" , "Helenu");
+
+// console.log(names)
+
+const push1  = names.push("Keka");
+
+console.log(names)
+//showing which index is injecting
+// console.log(push1)
+
+//removing elements
+// const pop1=names.pop();
+
+// console.log(names)
+//showing which one is poping
+// console.log(pop1)
+
+// searching index in an array
+// console.log(names.indexOf("Raina"))
+// searching element in an array
+// console.log(names.includes("Raj"))
