af17753dd42eb06da3424eca043f5bab5aeeb002
mujahidraj <mujahidraj65@gmail.com>
2025-07-24 12:34:04 +0600

    18.8

---
+++ Module-19/19.8_for.js
@@ -0,0 +1,26 @@
+
+// Subtask-1:
+
+// Display sum of all the odd numbers from 91 to 129.
+
+
+
+// Subtask-2:
+
+// Display sum of all the even numbers from 51 to 85.
+
+// subtask 1
+
+let sum=0;
+// for(let i=91;i<=129;i+=2){
+//     sum+=i;
+// }
+// console.log(sum)
+
+// subtask 2
+
+for(let i=51;i<=85;i+=2){
+    sum+=i;
+}
+
+console.log(sum)
