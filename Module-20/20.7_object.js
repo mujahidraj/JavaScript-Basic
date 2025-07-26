31fba598fb9ee90c72194bbdc0fd731ad41331c7
mujahidraj <mujahidraj65@gmail.com>
2025-07-27 00:29:10 +0600

    20.7

---
+++ Module-20/20.7_object.js
@@ -0,0 +1,26 @@
+// ### Task-2
+// For this object below add a property named `passenger capacity` with value 5
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+
+car["passenger capacity"] = 5;
+car.color="red";
+
+car.weight=[43,54,76,87];
+
+car.passengers = {list :{
+    name : "raj",
+    age:40
+}
+};
+
+console.log(car)
+
+console.log(car.passengers.list.name)
+
+console.log(car.weight)
+
