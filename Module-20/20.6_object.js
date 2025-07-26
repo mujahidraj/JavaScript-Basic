ffb0a937510489fd7dd68c2d4820d23005accbf2
mujahidraj <mujahidraj65@gmail.com>
2025-07-27 00:17:20 +0600

    20.6

---
+++ Module-20/20.6_object.js
@@ -0,0 +1,25 @@
+// ### Task-1
+// Access the `golden rod` color value in output.
+
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+};
+
+const myself = new Object ({name : "raj"},{age:25},{weight:75})
+
+const herself = Object.create({name : "mayad" ,age:24,"weight in kg":45})
+
+herself["weight in kg"] = 50
+
+console.log(herself["weight in kg"])
+
+
+
+console.log(myself.name)
+
+console.log(colors["golden rod"])
+
+console.log(colors.red)
