bb3460a5d8eb8d169104b31fa9583c787d3f99b2
mujahidraj <mujahidraj65@gmail.com>
2025-07-29 01:38:34 +0600

    21.1

--- 7 . Fundamental Concept of Functions/20.1_function.js
+++ 7 . Fundamental Concept of Functions/20.1_function.js
@@ -1,2 +1,11 @@
 // ### Task-1  
 // Take four parameters. Multiply the four numbers and then return the result 
+
+function multiply (num1 , num2 , num3 , num4){
+ 
+      let  result = num1 * num2* num3 * num4;
+    
+    return result;
+}
+
+console.log(multiply(1 ,2 ,3 ,4))
