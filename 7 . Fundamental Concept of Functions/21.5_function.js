46cca215c7392d2b5d1a0e8815b681239bdb1ae4
mujahidraj <mujahidraj65@gmail.com>
2025-07-29 17:02:25 +0600

    21.5

--- 7 . Fundamental Concept of Functions/21.5_function.js
+++ 7 . Fundamental Concept of Functions/21.5_function.js
@@ -1,2 +1,13 @@
 // ### Task-5 
 // Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return `Even`. If odd return `Odd`
+
+function odd_even(number){
+    if (number%2 ===0){
+        return "even";
+    }
+    else{
+        return "Odd";
+    }
+}
+
+console.log(odd_even(7))
