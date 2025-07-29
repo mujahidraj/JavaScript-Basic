bdbe1af6dd60eddd76816819605d553140cb7ede
mujahidraj <mujahidraj65@gmail.com>
2025-07-29 16:59:53 +0600

    21.4

--- 7 . Fundamental Concept of Functions/21.4_function.js
+++ 7 . Fundamental Concept of Functions/21.4_function.js
@@ -1,3 +1,19 @@
 
 // ### Task-4  
 // Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
+
+function count_zero(zeros) {
+    let count = 0;    
+    for (let i = 0; i < zeros.length; i++) {
+        if (zeros[i] == "0") {
+            count++
+        } 
+    }
+     return count;
+}
+
+let binaryString = "10100010";
+
+console.log(count_zero(binaryString));
+
+
