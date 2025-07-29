fc660b0f4635c0af0cde03628df5d25f46981b99
mujahidraj <mujahidraj65@gmail.com>
2025-07-29 16:49:15 +0600

    21.3

--- 7 . Fundamental Concept of Functions/21.3_function.js
+++ 7 . Fundamental Concept of Functions/21.3_function.js
@@ -1,3 +1,22 @@
 
 // ### Task-3
 // Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+
+function make_avg(array){
+let sum=0;
+for (let i = 0; i < arrays.length; i++) {
+    sum = sum + arrays[i];
+}
+return sum/array.length;
+}
+
+let arrays = [2,4,6,8,10];
+
+console.log(make_avg(arrays));
+
+
+
+
+
+
+
