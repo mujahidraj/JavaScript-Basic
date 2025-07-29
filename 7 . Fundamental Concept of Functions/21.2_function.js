f8d8b5b4667ac63e84e5c3c78b74475878bbc486
mujahidraj <mujahidraj65@gmail.com>
2025-07-29 16:38:35 +0600

    21.2

--- 7 . Fundamental Concept of Functions/21.2_function.js
+++ 7 . Fundamental Concept of Functions/21.2_function.js
@@ -1,2 +1,13 @@
 // ### Task-2
 // Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+
+function odd_even_multi (number){
+    if(number%2===0){
+        return number/2;
+    }
+    else{
+        return number*2;
+    }
+}
+
+console.log(odd_even_multi(16))
