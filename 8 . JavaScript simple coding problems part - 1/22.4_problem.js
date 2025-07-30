1800910389c1fc5950d1a6a78fec03ce2ba27096
mujahidraj <mujahidraj65@gmail.com>
2025-07-30 22:26:38 +0600

    22.4_problem

--- 8 . JavaScript simple coding problems part - 1/22.4_problem.js
+++ 8 . JavaScript simple coding problems part - 1/22.4_problem.js
@@ -7,3 +7,28 @@
 
 // sample-output: Programming
 
+function longest_word (strings){
+
+    let longestWord = " ";
+
+    let splitted = strings.split(" ")
+
+    console.log(splitted)
+
+    for(const words of splitted){
+
+        if(words.length> longestWord.length){
+            longestWord = words;
+        }
+
+    }
+
+    return longestWord;
+
+}
+
+let mainString = "I am learning Programming to become a programmer";
+
+console.log(longest_word(mainString));
+
+
