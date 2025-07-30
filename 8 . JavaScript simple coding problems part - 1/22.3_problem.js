41dd1f29607f29b9963b5df8131a769fa023bd24
mujahidraj <mujahidraj65@gmail.com>
2025-07-30 22:09:11 +0600

    22.3_problem

--- 8 . JavaScript simple coding problems part - 1/22.3_problem.js
+++ 8 . JavaScript simple coding problems part - 1/22.3_problem.js
@@ -2,3 +2,20 @@
 // ----
 // ### Task-3: 
 // Write a function to count the number of vowels in a string.
+
+function vowels(strs){
+
+    let count =0;
+
+    for(let i=0; i<=strs.length;i++){
+        if(strs[i]==="a" || strs[i]==="e" || strs[i]==="i" || strs[i]==="o"|| strs[i]==="u"){
+            count++;
+        }
+    }
+
+    return count;
+}
+
+let mainString = "Write a function to count the number of vowels in a string";
+
+console.log(vowels(mainString));
