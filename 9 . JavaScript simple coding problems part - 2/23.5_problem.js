6257c40007b2533a316cfd45856e8cd2a3b7c43a
mujahidraj <mujahidraj65@gmail.com>
2025-07-31 20:24:46 +0600

    23.1_problem

--- 9 . JavaScript simple coding problems part - 2/23.5_problem.js
+++ 9 . JavaScript simple coding problems part - 2/23.5_problem.js
@@ -9,4 +9,27 @@
 //         ];
 // ```
 
+function salaryCalculation (employeeInformation){
+    let totalIncrement = 0;
+    let employeeTotalSalary =0;
 
+    for(const employee of employeeInformation){
+        employeeTotalSalary = (employee.experience * employee.increment) + employee.starting ;
+        totalIncrement += employeeTotalSalary;
+    }
+
+    return totalIncrement;
+
+
+}
+
+
+ const employees = [
+            { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+            { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+            { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+            { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+        ];
+
+
+        console.log(salaryCalculation(employees))
