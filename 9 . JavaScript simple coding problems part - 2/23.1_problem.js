b4c32be95d709991961afe7e6d0805352e2c5b80
mujahidraj <mujahidraj65@gmail.com>
2025-07-31 20:00:18 +0600

    23.1_problem

--- 9 . JavaScript simple coding problems part - 2/23.1_problem.js
+++ 9 . JavaScript simple coding problems part - 2/23.1_problem.js
@@ -5,3 +5,16 @@
 
 // ---
 
+function lowestNumber(numbers) {
+    let lowestNumbers = numbers[0];
+    for (const number of numbers) {
+        if (lowestNumbers > number) {
+            lowestNumbers = number;
+        }
+    }
+    return lowestNumbers;
+}
+
+const heights = [54, 76, 54, 21, 87, 9, 65];
+
+console.log(lowestNumber(heights));
