532860cc9e5c50aa67155c2eea372375c63ac32a
mujahidraj <mujahidraj65@gmail.com>
2025-07-31 19:56:10 +0600

    problems

---
+++ 9 . JavaScript simple coding problems part - 2/23.1_problem.js
@@ -0,0 +1,7 @@
+// ### Task -1: 
+// Find the lowest number in the array below.
+// <br>
+// `const heights2 = [167, 190, 120, 165, 137];`
+
+// ---
+
---
+++ 9 . JavaScript simple coding problems part - 2/23.2_problem.js
@@ -0,0 +1,7 @@
+// ### Task -2: 
+// Find the friend with the smallest name.
+// <br>
+// `const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];`
+
+// ---
+
