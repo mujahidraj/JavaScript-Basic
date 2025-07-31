35be9c034d0bf89f682057e5c7c6c791ce34d1b3
mujahidraj <mujahidraj65@gmail.com>
2025-07-31 15:40:43 +0600

    smallest number

--- 9 . JavaScript simple coding problems part - 2/tallest.js
+++ 9 . JavaScript simple coding problems part - 2/tallest.js
@@ -10,6 +10,20 @@ function tallest (height){
     return tallestHeight;
 }
 
+function smallest (height){
+    
+        smallestHeight=height[0]
+
+    for(const num of height){
+        if(smallestHeight>num){
+            smallestHeight=num;
+        }
+    }
+    return smallestHeight;
+}
+
 const heights = [43,76,65,97,65,87,95,32,54]
 
 console.log(tallest(heights))
+
+console.log(smallest(heights));
