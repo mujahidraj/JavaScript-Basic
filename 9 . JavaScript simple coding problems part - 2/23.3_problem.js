61167f2a861d1bafdbcf479a60bb60e6566bc4c4
mujahidraj <mujahidraj65@gmail.com>
2025-07-31 20:09:51 +0600

    23.3_problem

--- 9 . JavaScript simple coding problems part - 2/23.3_problem.js
+++ 9 . JavaScript simple coding problems part - 2/23.3_problem.js
@@ -7,3 +7,18 @@
 
 // Write a JavaScript function named `calculateElectronicsBudget` that takes in the number of `laptop, tablets, and mobile` and returns the `total money required`.
 
+function calculateElectronicsBudget (budget1, budget2 , budget3){
+
+    let totalMoneyRequired =0;
+    if(typeof budget1 ==="number" && typeof budget2 ==="number" && typeof budget3 ==="number"){
+        totalMoneyRequired = budget1 + budget2 + budget3 ;
+
+    }
+    return totalMoneyRequired; 
+}
+
+const laptop = 35000;
+const tablet = 15000;
+const mobile = 20000;
+
+console.log(calculateElectronicsBudget(laptop , tablet , mobile));
