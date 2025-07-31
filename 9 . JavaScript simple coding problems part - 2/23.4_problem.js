07dec448fecd30ee70af90ed7f7e1a49edb6fd2e
mujahidraj <mujahidraj65@gmail.com>
2025-07-31 20:16:30 +0600

    23.4_poroblem

--- 9 . JavaScript simple coding problems part - 2/23.4_problem.js
+++ 9 . JavaScript simple coding problems part - 2/23.4_problem.js
@@ -17,3 +17,25 @@
 
 // ---
 
+
+function findAveragePhonePrice (mobiles){
+    let sum = 0;
+
+    for (const mobile of mobiles){
+        sum += mobile.price;
+    }
+    return Math.round(sum/mobiles.length);
+}
+
+
+const phones = [
+            { model: "PhoneA", brand: "Iphone", price: 95000 },
+            { model: "PhoneB", brand: "Samsung", price: 40000 },
+            { model: "PhoneC", brand: "Oppo", price: 26000 },
+            { model: "PhoneD", brand: "Nokia", price: 35000 },
+            { model: "PhoneE", brand: "Iphone", price: 105000 },
+            { model: "PhoneF", brand: "HTC", price: 48000 },
+        ];
+
+    console.log(findAveragePhonePrice(phones));
+    
