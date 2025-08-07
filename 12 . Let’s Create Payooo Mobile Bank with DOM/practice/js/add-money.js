514152bae125ead174281e111f8ea21ffaf3cf2e
mujahidraj <mujahidraj65@gmail.com>
2025-08-07 17:40:13 +0600

    cashed out

--- 12 . Let’s Create Payooo Mobile Bank with DOM/practice/js/add-money.js
+++ 12 . Let’s Create Payooo Mobile Bank with DOM/practice/js/add-money.js
@@ -7,6 +7,40 @@ document.getElementById("cash-out-button").addEventListener("click", function (e
     document.getElementById("transfer-money").style.display = "none";
     document.getElementById("bonus").style.display = "none";
 
+    document.getElementById("withdraw-request").addEventListener("click", function (event) {
+        event.preventDefault()
+
+        const pin = document.getElementById("withdraw-pin").value;
+        if (pin === "1234") {
+            const balance = document.getElementById("balance");
+
+            const amountRequest = document.getElementById("withdraw-ammount").value;
+
+            const totalBalance = parseInt(balance.innerText) - parseInt(amountRequest);
+
+            console.log(totalBalance);
+
+            balance.innerText = totalBalance;
+            confirm(amountRequest + " USD cashed out from your account. \n Your main balanace is " + totalBalance + "USD Thank you.")
+        }
+
+        else {
+
+            const newPara = document.createElement("p");
+            newPara.id = "error";
+            newPara.innerText = "Wrong account number or pin number! \n Please enter the 4 digit pin number";
+            newPara.style.color = "red";
+            newPara.style.fontSize = "16px";
+            newPara.style.textAlign = "center";
+            newPara.style.paddingBottom = "20px";
+            document.getElementById("cash-out").appendChild(newPara);
+        }
+
+
+
+    })
+
+
 
 })
 
@@ -35,7 +69,7 @@ document.getElementById("add-money-buttons").addEventListener("click", function 
             console.log(totalBalance);
 
             balance.innerText = totalBalance;
-            confirm( amountRequest + " USD added to your account. \n Your main balanace is " + totalBalance + "USD Thank you.")
+            confirm(amountRequest + " USD added to your account. \n Your main balanace is " + totalBalance + "USD Thank you.")
         }
 
         else {
@@ -65,6 +99,40 @@ document.getElementById("transfer-money-button").addEventListener("click", funct
     document.getElementById("cash-out").style.display = "none";
     document.getElementById("add-money").style.display = "none";
     document.getElementById("bonus").style.display = "none";
+
+    document.getElementById("send-request").addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const pin = document.getElementById("send-pin").value;
+        if (pin === "1234") {
+            const balance = document.getElementById("balance");
+
+            const amountRequest = document.getElementById("send-request").value;
+
+            const totalBalance = parseInt(balance.innerText) - parseInt(amountRequest);
+
+            console.log(totalBalance);
+
+            balance.innerText = totalBalance;
+            confirm(amountRequest + " USD cashed out from your account. \n Your main balanace is " + totalBalance + "USD Thank you.");
+        }
+
+        else {
+
+            const newPara = document.createElement("p");
+            newPara.id = "error";
+            newPara.innerText = "Wrong account number or pin number! \n Please enter the 4 digit pin number";
+            newPara.style.color = "red";
+            newPara.style.fontSize = "16px";
+            newPara.style.textAlign = "center";
+            newPara.style.paddingBottom = "20px";
+            document.getElementById("cash-out").appendChild(newPara);
+        }
+
+
+
+    })
+
 
 })
 
