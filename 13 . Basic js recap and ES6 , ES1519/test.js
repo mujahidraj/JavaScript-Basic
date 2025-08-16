9ae3c921317829a0dbf0143edd27ebe98ff9f119
mujahidraj <mujahidraj65@gmail.com>
2025-08-16 21:43:26 +0600

    hello

---
+++ 13 . Basic js recap and ES6 , ES1519/test.js
@@ -0,0 +1,16 @@
+
+//this a arrow function that can be used to simplifies the function
+const multiply = (number1 , number2) => number1 + number2;
+
+console.log(multiply(2,2))
+
+// string can be written in the double qoutation , single qoutation or in the backtick 
+//string in the backtick can be easier to use as it can make a string multiple line , double line , space line or any variable can be written 
+
+const aString = `hello my name is raj
+and i am a web developer`;
+
+const bString = `${aString} and also a frontend focued full stack developer.`
+
+console.log(bString);
+
