fda979e618e10b4ea445a7c49a17cfe06695b398
mujahidraj <mujahidraj65@gmail.com>
2025-08-21 23:30:11 +0600

    spread oparator and destructuring

--- 13 . Basic js recap and ES6 , ES1519/test.js
+++ 13 . Basic js recap and ES6 , ES1519/test.js
@@ -1,8 +1,8 @@
 
 //this a arrow function that can be used to simplifies the function
-const multiply = (number1 , number2) => number1 + number2;
+const multiply = (number1, number2) => number1 + number2;
 
-console.log(multiply(2,2))
+// console.log(multiply(2,2))
 
 // string can be written in the double qoutation , single qoutation or in the backtick 
 //string in the backtick can be easier to use as it can make a string multiple line , double line , space line or any variable can be written 
@@ -12,5 +12,22 @@ and i am a web developer`;
 
 const bString = `${aString} and also a frontend focued full stack developer.`
 
-console.log(bString);
+// console.log(bString);
+
+const raj = {
+    names: "Moizuddin Mohammad Mujahid Rashid",
+    age: 45,
+    semester: "12th",
+
+}
+
+const { age } = raj;
+
+console.log(age);
+
+const array = [12, 4, 56, 8, 34, 2, 124, 5, 3, 21, 5];
+
+const array2 = [12, 23, ...array, 1, 2,3, 4, 5,6]
+
+console.log(array2);
 
