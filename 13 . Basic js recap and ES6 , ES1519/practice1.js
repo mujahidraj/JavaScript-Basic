80c4ccc9c16544ae449ceed503f4fd5c4e1c107c
mujahidraj <mujahidraj65@gmail.com>
2025-08-22 21:09:59 +0600

    practice 1

--- 13 . Basic js recap and ES6 , ES1519/practice1.js
+++ 13 . Basic js recap and ES6 , ES1519/practice1.js
@@ -1,3 +1,15 @@
 const multiply =(num1 , num2, num3) => num1*num2*num3;
 
 console.log(multiply(2,3,4));
+
+
+const mainString = `I am a developer.
+i love coding.
+i love to eat biriyani.`;
+
+console.log(mainString);
+
+const sum =(num1=10 ,num2=10) => num1+num2;
+
+console.log(sum(20));
+
