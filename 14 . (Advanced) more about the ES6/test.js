e34a60bf2fe98f4fac22ce14ac84a27c0440a667
mujahidraj <mujahidraj65@gmail.com>
2025-08-24 18:47:43 +0600

    chaining

---
+++ 14 . (Advanced) more about the ES6/test.js
@@ -0,0 +1,32 @@
+// to acess the object property which is inside the array we can acess by this .
+const data = [{name : "hello world", id :1 ,age:12,price :100}]
+
+// console.log(data[0].age);
+
+
+const product = {
+    name : "Laptop",
+    details : [{price : 12000,model : "HP"},
+            {price: 13000,model :"Dell"},
+            {price : 130000, model : "macbook"}
+    ]
+}
+
+// console.log(product.details[0].model);
+
+
+const person = {
+    name : "Raj",
+    age : 23,
+    address : {
+        city : "Dhaka",
+        country : "bangladesh",
+        street : {
+            first : "Kuratoli",
+            second : "AIUB Road"
+        } 
+    }
+}
+
+console.log(person.address.street.first);
+
