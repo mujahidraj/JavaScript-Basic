570f54e27aac390beb9abc932f2d374026571225
mujahidraj <mujahidraj65@gmail.com>
2025-08-27 23:16:37 +0600

    practice 2

--- 14 . (Advanced) more about the ES6/practice-3.js
+++ 14 . (Advanced) more about the ES6/practice-3.js
@@ -1,7 +1,7 @@
 const instructors = [
-  {name: 'Nodi', age: 28, position: 'Senior'},
-  {name: 'Akil', age: 26, position: 'Junior'},
-  {name: 'Shobuj', age: 30, position: 'Senior'},
+  {name: 'Raj', age: 26, position: 'Senior'},
+  {name: 'Mayad', age: 25, position: 'Junior'},
+  {name: 'Raina', age: 20, position: 'Senior'},
 ];
 
 
