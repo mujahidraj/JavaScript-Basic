06c2eb3e8328303828e0694fcb45f30524a32a9b
mujahidraj <mujahidraj65@gmail.com>
2025-08-30 17:53:10 +0600

    true and false

---
+++ 15 . Understand common JavaScript Problem/test.js
@@ -0,0 +1,20 @@
+// javascript called dynamic type programming language . beacause javascript dont need to declare the data type to declare the variable . 
+
+// number , string , boolean these are the primitive type 
+// array and object is the non - primitive type 
+
+// non primitive hold the reference in the time of value change not reassign
+
+// there is 8 ways to get the undefined value 
+// 1 . value that is not initialized  
+// 2 . function with no return 
+// 3 . parameter that is not passed
+// 4 . return that has nothing on the return's right side 
+// 5 . property that is not exist in the object 
+// 6 . accessing a array element outside of the array range 
+// 7 . deleting an element inside a array 
+// 8 . set a value directly to undefined
+
+// difference between truthy and falsy
+// truthy : 1 . true ,2 . any number (postive or negative) will be truthy except zero 3 . any string is truthy except empty . 4. "0" 5 . "false" or "true"
+// falsy : 1 . false , 2 . empty string 3 . 0 4 . undefine is false 5 . null is false 
