a39904bf97edd52a01d3f9494a7ef10a31f3846e
mujahidraj <mujahidraj65@gmail.com>
2025-09-08 21:25:31 +0600

    stringify

--- 16 . API,JSON,Data load and dynamic website/test.js
+++ 16 . API,JSON,Data load and dynamic website/test.js
@@ -1,1 +1,28 @@
 // https is a secure encrypted system 
+
+// json = JavaScript Object Notation 
+
+const person = {
+    name : {
+        first : "Moizuddin",
+        second : "Mohammad",
+        third : "Mujahid",
+        Forth : "Rashid",
+        fifth : "Raj"
+    },
+    address : "Dhaka",
+    age : 25,
+    isAdult : true
+}
+
+// using the jason.stringify we can convert the javascript values into the jason string and json .
+
+console.log(person);
+console.log(typeof person);
+
+const personJSON = JSON.stringify(person);
+
+console.log(personJSON);
+console.log(typeof personJSON);
+
+
