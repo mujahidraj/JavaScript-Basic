8bbfb973e1b3e76c042dacd8a2dec3151b735827
mujahidraj <mujahidraj65@gmail.com>
2025-09-10 00:10:55 +0600

    module 34

---
+++ 17 . More about JavaScript/test.js
@@ -0,0 +1,33 @@
+// javascript is a highly abstracted programming language
+// an abstraction is a way of hiding the implementation details and showing only the functionality to the users 
+// we dont have to....
+/*
+handle resource management and memory allocation
+hanfle garbage collection
+doveloper can focus on logics
+improve understandability and maintainability 
+*/
+
+// javascript is not an interpreted language
+// mordern js is JIT compiled 
+// JIT convert the code intp the machine code and execute them imediately 
+
+
+
+// js is a multi paradigm programming language
+/*
+procedural programming
+object oriented programming
+functional programming
+*/
+
+// js is a prototype based programming language
+// js is a dynamically type programming language
+
+// execution context is a envoironment 
+// in execution context a piece of javascript code gets executed
+
+// everything is store in the execution context
+// there are two type of execution context 
+// global execution context
+// function execution context
