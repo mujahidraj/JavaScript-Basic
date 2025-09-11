6df23e6e6fd5695d6e738a7917852a0bf363c92a
mujahidraj <mujahidraj65@gmail.com>
2025-09-11 20:20:35 +0600

    trow

--- 17 . More about JavaScript/try-catch/try-catch.js
+++ 17 . More about JavaScript/try-catch/try-catch.js
@@ -1,6 +1,17 @@
 // try catch works like if there is an error in the try section then it will encountered an error . and execute the others code . but if there is no try catch it will not execute the other code 
 try{
-console.log(names);
+if(!person){
+    console.log(person);
+
+    // throw work like if there is a if else or other thing if there is multiple error possibilities then it will throw a error imediately 
+    // we can throw a exception like exception can be tring , number , boolean , object  
+throw "please defined the name";
+
+console.log(address);
+
+}
+
+
 }
 // if there is a error in the try section then it will catch the errro and show us 
 catch(err){
