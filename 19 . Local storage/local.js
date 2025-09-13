5407d0b5cc4ad6aedd762975410b5bb4744626bb
mujahidraj <mujahidraj65@gmail.com>
2025-09-13 17:42:22 +0600

    local Storage

---
+++ 19 . Local storage/local.js
@@ -0,0 +1,6 @@
+const setItems = ()=>{
+    window.localStorage.setItem("Name" , "Raj")
+    window.localStorage.setItem("IsDark" , true)
+    window.localStorage.setItem("age" , 25)
+    
+}
---
+++ 19 . Local storage/test.js
@@ -0,0 +1,4 @@
+//  we can get the local storage from the chrome browsers devtools application 
+// we can get the key value using the localstorage.getItem("keyName") 
+// to set the value of the key we can simple write localstorage.setItem("keyName","KeyValue")
+// to remove item we simple write localstorage.removeItem("keyName")
