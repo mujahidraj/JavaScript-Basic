0d784e3f99a9e0fa8519c50721b87a5ba8026717
mujahidraj <mujahidraj65@gmail.com>
2025-09-16 16:54:47 +0600

    new module

---
+++ 20 . JavaScript you need to know before reactjs/test.js
@@ -0,0 +1,21 @@
+// things that we need to know before reactjs
+/*
+
+1 . variable : var , let , const
+2 . if else 
+3 . array
+4 . length
+5 . push pop
+6 . shift unshift
+7 . slice 
+8 . splice
+9 . split
+10 . for loop
+11 . for of
+12 . for in
+13 . function 
+14 . spread
+15 . arrow function 
+16 . object 
+
+*/
